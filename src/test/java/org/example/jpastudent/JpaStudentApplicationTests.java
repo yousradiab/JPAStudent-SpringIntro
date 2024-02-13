c44558e3b42aa6b75066db79a1acf6aa6f1eb590
yousradiab <yousserah@gmail.com>
2024-02-13 09:21:18 +0100

    JPAtest

--- src/test/java/org/example/jpastudent/JpaStudentApplicationTests.java
+++ src/test/java/org/example/jpastudent/JpaStudentApplicationTests.java
@@ -1,9 +1,11 @@
 package org.example.jpastudent;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
+
 class JpaStudentApplicationTests {
 
     @Test
--- src/test/java/org/example/jpastudent/repository/StudentRepositoryTest.java
+++ src/test/java/org/example/jpastudent/repository/StudentRepositoryTest.java
@@ -3,13 +3,15 @@ package org.example.jpastudent.repository;
 import org.example.jpastudent.model.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
+
 class StudentRepositoryTest {
 
     @Autowired
@@ -20,15 +22,15 @@ class StudentRepositoryTest {
         List<Student> lst = studentRepository.findAllByName("Nanna");
 
         //forventer der er 1 Nanna i database
-        assertEquals(4, lst.size());
+        assertEquals(0, lst.size());
     }
 
     @Test
     void testOnetime2() {
         List<Student> lst = studentRepository.findAllByName("Nanna");
 
-        //forventer der er 1 Nanna i database
-        assertEquals(4, lst.size());
+        //forventer der er 4 Nanna i database
+        assertEquals(0, lst.size());
     }
 
 }
